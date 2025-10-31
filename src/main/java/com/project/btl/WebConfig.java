2aeb2fa337d4397a4df1dc46fecd0fd557dbbed9
anhphuc0508 <PhucNPA.B23CE078@stu.ptit.edu.vn>
2025-10-31 07:24:24 +0700

    thêm WebConfig kết nối với FE

---
+++ src/main/java/com/project/btl/WebConfig.java
@@ -0,0 +1,18 @@
+package com.project.btl;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // cho phép React gọi API
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
