abf8c9b7b7174184dcb9935936f0d9d3cd044c1b
anhphuc0508 <PhucNPA.B23CE078@stu.ptit.edu.vn>
2025-11-26 02:08:44 +0700

    đaowa

--- src/main/java/com/project/btl/config/SecurityConfig.java
+++ src/main/java/com/project/btl/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000" , "https://oop-123.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("https://gymsup.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
